47af4f70ec97ddb01afb626a4ed6474b2671d52d
mugambi645 <mugambi645@gmail.com>
2021-11-17 22:16:20 +0300

    added validation to contact form

--- app/FAQS/views.py
+++ app/FAQS/views.py
@@ -1,7 +1,7 @@
 from . import faqs as f
 from flask_login import login_required,current_user
 from .forms import ContactForm
-from flask import render_template,abort,redirect,url_for,request
+from flask import render_template,abort,redirect,url_for,request,flash
 @f.route("/faqs")
 def bussiness_idea():
     return render_template("FAQS/bussiness.html")
@@ -9,8 +9,14 @@ def bussiness_idea():
 
 @f.route('/contact', methods=['GET', 'POST'])
 def contact():
-    form = ContactForm()
-    if request.method == 'POST':
-        return 'Form posted.'
-    elif request.method == 'GET':
-        return render_template('FAQS/contact.html', form=form)
+  form = ContactForm()
+
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('FAQS/contact.html', form=form)
+    else:
+      return 'Form posted.'
+
+  elif request.method == 'GET':
+    return render_template('FAQS/contact.html', form=form)
