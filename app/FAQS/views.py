e6c5b422c5729741130422cd9e596b173a97cfdf
mugambi645 <mugambi645@gmail.com>
2021-11-17 22:07:49 +0300

    import contact form instance from forms

--- app/FAQS/views.py
+++ app/FAQS/views.py
@@ -1,5 +1,6 @@
 from . import faqs as f
 from flask_login import login_required,current_user
+from .forms import ContactForm
 from flask import render_template,abort,redirect,url_for,request
 @f.route("/faqs")
 def bussiness_idea():
