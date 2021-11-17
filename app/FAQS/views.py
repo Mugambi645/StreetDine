c7ec5cc16a9d2d25e7230aaaac9620fa9aeb28bc
mugambi645 <mugambi645@gmail.com>
2021-11-17 22:30:20 +0300

    add about.html page

--- app/FAQS/views.py
+++ app/FAQS/views.py
@@ -7,16 +7,6 @@ def bussiness_idea():
     return render_template("FAQS/bussiness.html")
 
 
-@f.route('/contact', methods=['GET', 'POST'])
-def contact():
-  form = ContactForm()
-
-  if request.method == 'POST':
-    if form.validate() == False:
-      flash('All fields are required.')
-      return render_template('FAQS/contact.html', form=form)
-    else:
-      return 'Form posted.'
-
-  elif request.method == 'GET':
-    return render_template('FAQS/contact.html', form=form)
+@f.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template("FAQS/about.html")
