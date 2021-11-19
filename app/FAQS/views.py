fcd204cc9b08632c2cc1e6fea537672a7d8939a0
mugambi645 <mugambi645@gmail.com>
2021-11-19 08:09:40 +0300

    about page changes

--- app/FAQS/views.py
+++ app/FAQS/views.py
@@ -1,7 +1,7 @@
 from . import faqs as f
-from flask_login import login_required,current_user
+from flask_login import login_required,current_user,session_protected
 from .forms import ContactForm
-from flask import render_template,abort,redirect,url_for,request,flash
+from flask import render_template,abort,redirect,url_for,request,flash,session
 @f.route("/faqs")
 def bussiness_idea():
     return render_template("FAQS/bussiness.html")
@@ -10,3 +10,16 @@ def bussiness_idea():
 @f.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template("FAQS/about.html")
+
+
+@f.route('/about', methods=['GET', 'POST'])
+def contact():
+    session['firstname'] = request.form.get('firstname')
+    session['lastname'] = request.form.get('lastname')
+    session['email'] = request.form.get('email')
+    session['subject'] = request.form.get('subject')
+    session['message'] = request.form.get('message')
+    print(session['firstname'], session['lastname'], session['email'], session['subject'],session['message'])
+    if session['firstname'] != None:
+        flash(f'Message sent!', 'success')
+    return render_template('FAQS/about.html', tilte = 'Contact')
